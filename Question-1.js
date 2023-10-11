2f69459f6c4a45c3293d6d2002749d2aa2b86848
shankarks96 <145577437+shankarks96@users.noreply.github.com>
2023-10-11 14:37:22 +0530

    Add files via upload

--- Question-1.js
+++ Question-1.js
@@ -1,22 +1,34 @@
+
 const data = {
-    name: "SHANKAR K S",
-    age: 23,
-    city: "CHENNAI"
-  };
-  
-  // Using a for loop
-  const keys = Object.keys(data);
-  for (let i = 0; i < keys.length; i++) {
-    const key = keys[i];
-    const value = data[key];
-    console.log(key + ": " + value);
-  }
-  
+  name: "SHANKAR K S",
+  age: 23,
+  city: "CHENNAI"
+};
 
-
+// Using a for loop
+const keys1 = Object.keys(data);
+for (let i = 0; i < keys1.length; i++) {
+  const key = keys1[i];
+  const value = data[key];
+  console.log(key + ": " + value);
+}
 
 // Using a for...in loop:
-
 for (let key in data) {
   console.log(key + ": " + data[key]);
 }
+
+// Using a for...of loop:
+const keys2 = Object.keys(data);
+for (let key of keys2) {
+  console.log(key + ": " + data[key]);
+}
+// Using a forEach loop:
+
+// Convert the object into an array of key-value pairs using Object.entries()
+const entries = Object.entries(data);
+
+// Use forEach to iterate over the key-value pairs
+entries.forEach(([key, value]) => {
+  console.log(key + ": " + value);
+});
