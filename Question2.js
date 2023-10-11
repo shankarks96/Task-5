8e11673f76e29385b0b0401e70e471134fff427d
shankarks96 <145577437+shankarks96@users.noreply.github.com>
2023-10-11 14:34:50 +0530

    test

---
+++ Question2.js
@@ -0,0 +1,47 @@
+const resumeData = {
+    name: "SHANKAR K S ",
+    contact: {
+      email: "shankar123@gmail.com",
+      phone: "+91 6383737143",
+      address: "12 Main Street, Adampakam, chennai"
+    },
+    objective: "Recent graduate seeking an entry-level position in software development to apply and further develop my programming skills",
+    education: [
+      {
+        degree: "Bachelor of Technology in Circuit Branch",
+        university: "srm University",
+        year: "2023"
+      }
+    ],
+    skills: [
+      "Programming languages: JavaScript, Python",
+      "Web development: HTML, CSS, React",
+      "Version control: Git",
+      "Problem-solving and debugging"
+    ],
+    projects: [
+      {
+        title: "Online Portfolio",
+        description: "Created a personal website to showcase my projects and skills using HTML, CSS, and JavaScript"
+      },
+      
+    ],
+    certifications: [
+      "On Going Certified Full Stack Web Development ((GUVI)Online Course)",
+      "Certified Software Testing Course (Q spider)"
+    ],
+    languages: [
+      "English (Fluent)",
+      "Tamil (Fluent)",
+      "Telugu (Fluent)"
+    ],
+    interests: [
+      "Web development",
+      "Open-source projects",
+      "Problem-solving"
+    ]
+};
+
+console.log(JSON.stringify(resumeData, null, 2));
+
+      
